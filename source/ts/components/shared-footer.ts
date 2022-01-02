0e511a62730210ca6d812b4d8c9873ada91dcd84
Bauke <me@bauke.xyz>
2022-01-02 21:36:00 +0100

    Add a link to the tree of the current commit.

--- source/ts/components/shared-footer.ts
+++ source/ts/components/shared-footer.ts
@@ -4,10 +4,15 @@ import ExternalAnchor from './external-anchor.js';
 
 export default class SharedFooter extends Component {
   render() {
+    const githubUrl = 'https://github.com/Bauke/blink';
+    const versionText = `v${blinkVersion}/${blinkCommitHash}`;
+    const versionUrl = `${githubUrl}/tree/${blinkCommitHash}`;
+
     return html`
       <footer class="shared-footer">
-        <${ExternalAnchor} text="GitHub" url="https://github.com/Bauke/blink" />
-        <span> v${blinkVersion} (${blinkCommitHash})</span>
+        <${ExternalAnchor} text="GitHub" url=${githubUrl} />
+        ${' '}
+        <${ExternalAnchor} text=${versionText} url=${versionUrl} />
       </footer>
     `;
   }
